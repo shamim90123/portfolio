7981f1cfa9b98782d550b5e13922cd77d04eb712
shamim901 <msreza901@gmail.com>
2021-01-31 23:54:18 +0600

    complate portfolio

--- main.js
+++ main.js
@@ -41,3 +41,69 @@ $('a.smooth-scroll')
     }
   }
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var pharModal = document.getElementById("pharModal");
+var agriModal = document.getElementById("agriModal");
+var bkModal = document.getElementById("bkModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var pharBtn = document.getElementById("pharBtn");
+var agriBtn = document.getElementById("agriBtn");
+var bkBtn = document.getElementById("bkBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var pharSpan = document.getElementsByClassName("close")[1];
+var agriSpan = document.getElementsByClassName("close")[2];
+var bkSpan = document.getElementsByClassName("close")[3];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+pharBtn.onclick = function() {
+  pharModal.style.display = "block";
+}
+
+agriBtn.onclick = function() {
+  agriModal.style.display = "block";
+}
+
+bkBtn.onclick = function() {
+  bkModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+pharSpan.onclick = function() {
+  pharModal.style.display = "none";
+}
+agriSpan.onclick = function() {
+  agriModal.style.display = "none";
+}
+bkSpan.onclick = function() {
+  bkModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  if (event.target == pharModal) {
+    pharModal.style.display = "none";
+  }
+  if (event.target == agriModal) {
+    agriModal.style.display = "none";
+  }
+  if (event.target == bkModal) {
+    bkModal.style.display = "none";
+  }
+}
+
